e7ea4b002ad1d6a6cb3890379e7a5f7dbc42dad5
Rainiery Valerio Gonzalez <rainieryvalerio@MacBook-Air-de-Rainiery.local>
2023-06-28 21:06:46 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SplashScreenPage } from './pages/splash-screen/splash-screen.page';
+import { HomePage } from './pages/home/home.page';
+import { CharacterDetailsPage } from './pages/character-details/character-details.page';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SplashScreenPage, CharacterDetailsPage],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, SplashScreenPage],
 })
 export class AppModule {}
